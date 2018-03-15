43a6dc8e11dc44df71dfc5804a7946dcb4b68035
qxu1994 <32419006+qxu1994@users.noreply.github.com>
2018-03-15 01:52:43 -0400

    use nltk to read corpus

---
+++ wdlen.py
@@ -0,0 +1,33 @@
+import nltk
+from nltk.corpus.reader import CHILDESCorpusReader
+import illegal_utterance_filter
+
+# C:\Users\ThinkPad\AppData\Roaming\nltk_data
+
+corpus_root = nltk.data.find( 'corpora/CHILDES/Eng-NA-MOR/')
+McCune = CHILDESCorpusReader(corpus_root, 'McCune/.*.xml')
+
+def wordlenth(corpus):
+    file = corpus.fileids()
+    wdlen_list = []
+    for f in file:
+        wdlen = {'fname': f, 'age': corpus.age(f, month=True)[0], 'one': 0, 'two': 0, 'three': 0, 'four': 0, 'five': 0}
+        for sent in corpus.sents(f, speaker='CHI'):
+            if not sent or not illegal_utterance_filter.utterance_filter(sent):  # in case there are empty utterances or utterances containing illegal ngrams.
+                continue
+            if len(sent) == 1:
+                wdlen['one'] += 1
+            elif len(sent) == 2:
+                wdlen['two'] += 1
+            elif len(sent) == 3:
+                wdlen['three'] += 1
+            elif len(sent) == 4:
+                wdlen['four'] += 1
+            elif len(sent) >= 5:
+                wdlen['five'] += 1
+        wdlen_list.append(wdlen)
+    return(wdlen_list)
+
+#corpus_root = nltk.data.find( 'corpora/CHILDES/Eng-NA-MOR/')
+#Manchester = CHILDESCorpusReader(corpus_root, 'Manchester/.*.xml')
+#print(wordlenth(Manchester))
