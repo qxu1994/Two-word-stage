d625a9df19d674bf0dac221b01030c815ce1471d
qxu1994 <32419006+qxu1994@users.noreply.github.com>
2018-04-09 13:34:11 -0400

    Add files via upload

--- illegal_utterance_filter.py
+++ illegal_utterance_filter.py
@@ -1,30 +1,48 @@
-ILLEGAL_NGRAMS = {
-    'uh oh',
-    'oh oh',
-    'oh dear',
-    'oh no',
-    'no no',
-    'a b',
-    'one two three',
-    'two three four',
-    'three four five',
-    'four five six',
-    'five six seven',
-    'six seven eight',
-    'seven eight nine',
-    'eight nine ten',
-    'nine ten eleven'}
+unintel = {
+    'XXX',
+    'YYY'}
 
+'''
+illegal = {
+    ['嗯'],
+    ['噢啊'],
+    ['哼'],
+    ['呷呷'],
+    ['啊呜'],
+    ['阿呜'],
+    ['噢呜'],
+    ['噢啊呵'],
+    ['呜噢']
+}
+'''
+
+
+'''
 def illegal_gram(gramstr):
     """ Returns false if ngram should be excluded from our analysis """
     if gramstr in ILLEGAL_NGRAMS:
         return True
-    return False
+    return False'''
 
-def utterance_filter(words):
+def unintel_filter(sent):
     """ Returns false if utterance should be excluded from our analysis """
+    for i in unintel:
+        if i in sent:
+            return False
+            break
+        else:
+            return True
+
+def illegal_filter(sent):
+    if sent in illegal:
+        return False
+    else:
+        return True
+
+
+    '''
     for i in range(2, 4):
         for j in range(0, len(words) - i + 1):
             if illegal_gram(' '.join(map(str, words[j:j+i]))):
                 return False
-    return True
+    return True'''
