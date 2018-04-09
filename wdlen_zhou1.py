c305c65bbfefabac110270126fb1c8839535765a
qxu1994 <32419006+qxu1994@users.noreply.github.com>
2018-04-09 13:35:09 -0400

    Add files via upload

--- wdlen_zhou1.py
+++ wdlen_zhou1.py
@@ -1,11 +1,9 @@
 import nltk
 from nltk.corpus.reader import CHILDESCorpusReader
-import illegal_utterance_filter
+from illegal_utterance_filter import unintel_filter
 
 # C:\Users\ThinkPad\AppData\Roaming\nltk_data
 
-corpus_root = nltk.data.find( 'corpora/CHILDES/Eng-NA-MOR/')
-McCune = CHILDESCorpusReader(corpus_root, 'McCune/.*.xml')
 def edit_age(age):
     if age == 21:
         Age = 20
@@ -21,10 +19,11 @@ def wordlenth(corpus):
     for f in file:
         wdlen = {'fname': f, 'age': edit_age(corpus.age(f, month=True)[0]), 'one': 0, 'two': 0, 'three': 0, 'four': 0, 'five': 0}
         for sent in corpus.sents(f, speaker='CHI'):
-            if not sent or not illegal_utterance_filter.utterance_filter(sent):  # in case there are empty utterances or utterances containing illegal ngrams.
+            if not sent or not unintel_filter(sent):  # in case there are empty utterances or utterances containing illegal ngrams.
                 continue
             if len(sent) == 1:
                 wdlen['one'] += 1
+                #print(sent)
             elif len(sent) == 2:
                 wdlen['two'] += 1
             elif len(sent) == 3:
@@ -35,3 +34,4 @@ def wordlenth(corpus):
                 wdlen['five'] += 1
         wdlen_list.append(wdlen)
     return(wdlen_list)
+
