ef8dbed08dc49084eda9e0035c175083c5effb7d
qxu1994 <32419006+qxu1994@users.noreply.github.com>
2018-04-09 13:34:36 -0400

    Add files via upload

--- wdlen.py
+++ wdlen.py
@@ -13,21 +13,26 @@ def wordlenth(corpus):
     for f in file:
         wdlen = {'fname': f, 'age': corpus.age(f, month=True)[0], 'one': 0, 'two': 0, 'three': 0, 'four': 0, 'five': 0}
         for sent in corpus.sents(f, speaker='CHI'):
-            if not sent or not illegal_utterance_filter.utterance_filter(sent):  # in case there are empty utterances or utterances containing illegal ngrams.
+            if not sent or not illegal_utterance_filter.unintel_filter(sent):  # in case there are empty utterances or utterances containing illegal ngrams.
                 continue
             if len(sent) == 1:
                 wdlen['one'] += 1
+                #print(sent)
             elif len(sent) == 2:
                 wdlen['two'] += 1
+                #print(sent)
             elif len(sent) == 3:
                 wdlen['three'] += 1
+                #print(sent)
             elif len(sent) == 4:
                 wdlen['four'] += 1
+                #print(sent)
             elif len(sent) >= 5:
                 wdlen['five'] += 1
+                #print(sent)
         wdlen_list.append(wdlen)
     return(wdlen_list)
 
-#corpus_root = nltk.data.find( 'corpora/CHILDES/Eng-NA-MOR/')
-#Manchester = CHILDESCorpusReader(corpus_root, 'Manchester/.*.xml')
+#corpus_root = nltk.data.find( 'corpora/CHILDES/Thai/')
+#Manchester = CHILDESCorpusReader(corpus_root, 'CRSLP/.*.xml')
 #print(wordlenth(Manchester))
