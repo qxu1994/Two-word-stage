a5dec0714467b290b57e0aa0e8e9caec7f3107a9
qxu1994 <32419006+qxu1994@users.noreply.github.com>
2018-12-16 02:39:42 -0500

    Add files via upload

--- illegal_utterance_filter.py
+++ illegal_utterance_filter.py
@@ -2,6 +2,7 @@ unintel = {
     'XXX',
     'YYY'}
 
+
 '''
 illegal = {
     ['嗯'],
@@ -13,8 +14,7 @@ illegal = {
     ['噢呜'],
     ['噢啊呵'],
     ['呜噢']
-}
-'''
+}'''
 
 
 '''
@@ -26,23 +26,64 @@ def illegal_gram(gramstr):
 
 def unintel_filter(sent):
     """ Returns false if utterance should be excluded from our analysis """
-    for i in unintel:
-        if i in sent:
-            return False
-            break
-        else:
-            return True
-
-def illegal_filter(sent):
-    if sent in illegal:
+    if sent == ['xxx'] or sent == ['yyy']:
         return False
     else:
         return True
 
+def unintel_filter_pos(sent):
+    """ Returns false if utterance should be excluded from our analysis """
+    if sent == ['xxx',''] or sent == ['yyy','']:
+        return False
+    else:
+        return True
 
-    '''
-    for i in range(2, 4):
-        for j in range(0, len(words) - i + 1):
-            if illegal_gram(' '.join(map(str, words[j:j+i]))):
-                return False
-    return True'''
+def unintel_filter_gra(sent):
+    """ Returns false if utterance should be excluded from our analysis """
+    if set(sent) == {('xxx','')} or set(sent) == {('yyy','')} or set(sent) == {('xxx',''), ('yyy', '')}:
+        return False
+    else:
+        return True
+
+def new(sent):
+    new_sent = []
+    for i in sent:
+        if i == 'xxx' or i == 'yyy' or i == 'ss' or i == 'ow' or i == 'bok':
+            continue
+        else:
+            new_sent.append(i)
+    if len(new_sent)==4 and len(set(new_sent))==2 or len(new_sent)-len(set(new_sent))>2 or len(new_sent) == 3:
+        new_sent = list(set(new_sent))
+    return new_sent
+
+def new_original(sent):
+    new_sent = []
+    for i in sent:
+        if i == 'xxx' or i == 'yyy' or i == 'ss' or i == 'ow' or i == 'bok':
+            continue
+        else:
+            new_sent.append(i)
+    return new_sent
+
+def new_original_gra(sent):
+    new_sent = []
+    for i in sent:
+        if i[0] == 'xxx' or i[0] == 'yyy' or i[0] == 'ss' or i[0] == 'ow' or i[0] == 'bok':
+            continue
+        else:
+            new_sent.append(i)
+    return new_sent
+
+def illegal_filter(sent, illegal):
+    if (len(new(sent))>1 and len(set(new(sent))) == 1) or (set(new(sent)) == {'xxx', 'yyy'}) or sent in illegal:
+        return False
+    else:
+        return True
+
+def illegal_filter_pos(sent, illegal):   # also suits the gra analysis
+    sent = [i[0] for i in sent]
+    if (len(new_original(sent))>1 and len(set(new_original(sent))) == 1) or (set(new_original(sent)) == {'xxx', 'yyy'}) or sent in illegal:
+        return False
+    else:
+        return True
+
